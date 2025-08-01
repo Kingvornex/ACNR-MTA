25590af0fea6c7eaa1f4f02d3095529d54984dcd
Kingvornex <161863111+Kingvornex@users.noreply.github.com>
2025-08-01 19:12:32 +0330

    Create config.lua

---
+++ shared/config.lua
@@ -0,0 +1,21 @@
+-- Database configuration
+DB_CONFIG = {
+    host = "localhost",
+    user = "root",
+    password = "",
+    database = "acnr"
+}
+
+-- Game settings
+MAX_PLAYERS = 100
+STARTING_MONEY = 5000
+STARTING_HEALTH = 100
+STARTING_ARMOR = 0
+SPAWN_X = 1958.3783
+SPAWN_Y = -1684.2764
+SPAWN_Z = 14.5469
+SPAWN_ANGLE = 270.0
+
+-- Vehicle settings
+MAX_VEHICLES = 200
+VEHICLE_RESPAWN_TIME = 30000 -- 30 seconds
